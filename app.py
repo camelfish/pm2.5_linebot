5d6f29bbb734e3476c7f8024cfa860bc54ad22e2
camelfish <camelfish23@gmail.com>
2018-05-29 09:39:20 +0800

    version_1

--- app.py
+++ app.py
@@ -9,6 +9,11 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
 )
+import requests
+import googlemaps
+import numpy as np
+import json
+import os
 
 app = Flask(__name__)
 
@@ -16,6 +21,11 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('+6Vcg6zNfbqzQTPZF3hx8svapB2CFA1QyTrObKNMMdcOS0WUsB7KNataArmQLHj4K/H9y99ayvbt5HGCLf85zkjcuCfAL35H8H1HhdkhhBszkwBYuIGClUZkqopuPKydtv2tGMDQ7bFlcFaAU4NbZQdB04t89/1O/w1cDnyilFU=')
 # Channel Secret
 handler = WebhookHandler('509073f22655c9158ec62aa059de1a32')
+#GoogleMap key
+gmaps = googlemaps.Client(key='AIzaSyB25oVe3hd7XD-0kG0vWyRKfmIyDAX1YsU')
+#google_map_api = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address=' +loc+ '&key=AIzaSyB25oVe3hd7XD-0kG0vWyRKfmIyDAX1YsU').json()
+
+data_pm = requests.get("https://pm25.lass-net.org/data/last-all-airbox.json").json()
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -36,15 +46,40 @@ def callback():
 
     return 'OK'
 
+loc_dis_min = 1000
+device_id_min = ''
+pm25_min = 0
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
+    
+    user_loc = event.message.text
+    gecode_result = gmaps.geocode(user_loc)
+    #the [lat, lon] of user_loc
+    user_loc_abs = np.array(list(gecode_result[0]['geometry']['location'].values()))
+
+    for i in data_pm['feeds']:
+        device_loc_abs = np.array([i['gps_lat'],i['gps_lon']])
+
+        #Euclidean Distance between two location
+        loc_dis=np.sqrt(np.sum(np.square(user_loc_abs-device_loc_abs)))
+
+        if loc_dis < loc_dis_min:
+            loc_dis_min = loc_dis
+            device_id_min = i['device_id']
+            pm25_min = i['s_d0']
+
+    reply_mes = '距離' +user_loc+ '最近的pm2.5是' +str(pm25_min)
+
+    output_mes = TextSendMessage(text=reply_mes)
+
     line_bot_api.reply_message(
         event.reply_token,
-        message)
+        output_mes)
 
-import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
+
+AIzaSyB25oVe3hd7XD-0kG0vWyRKfmIyDAX1YsU
